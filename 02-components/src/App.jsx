4666a21b7a3ccec3c763d58c3d8481f676b04344
vansh <guptadevansh417@gmail.com>
2026-03-16 11:31:40 +0530

    components done

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./components/Card.jsx";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default App;
---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+       <div className="card">
+        <h1>I am Vansh</h1>
+        <p>
+           China's first full-length feature film, Yan Ruisheng was
+          produced as domestic short films were becoming increasingly common. It
+          emphasized accuracy to real life in its casting and setting; the stars
+          were chosen based on their physical resemblance to those involved,
+          while extensive use of location shooting allowed scenes to be set in
+          places associated with the murder. A commercial success upon release,
+          the critical reception of its technical aspects was posit
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
